39c890f013de306a70d81341dc18267e2a11cf18
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-07-11 15:32:13 -0300

    change links in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
 import './App.css'
+import Navbar from './components/Navbar'
 
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
