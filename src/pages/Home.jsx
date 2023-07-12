222a9585a92f571463148e0715e9864af238f042
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-07-12 15:21:28 -0300

    add Home information and edit css for page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/Home.css'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <h2>Pedro Prado</h2>
+      <h3>Pedro Henrique Prado</h3>
+      <h1>Developer Frontend & IoT</h1>
     </div>
   )
 }
