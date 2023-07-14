738a15b447f14e6ca10b8ea1e3b6eb121b8beab0
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-07-14 09:37:39 -0300

    created projects page, and style for this

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import Projct1 from '../assets/firstproject_mac.png'
+import '../styles/Project.css'
 
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='projects'>
+      <h2>Projetos pessoais</h2>
+      <div className="projectList">
+        <ProjectItem name="Criar e compartilhar eventos" image={Projct1}/>
+        <ProjectItem name="Portifólio Med Veterinária"/>
+        <ProjectItem/>
+        <ProjectItem/>
+
+      </div>
     </div>
   )
 }
